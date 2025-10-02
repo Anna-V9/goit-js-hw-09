de32efd4466192eadc43630971440d123170eaff
Anna <tranina.ann@gmail.com>
2025-10-02 17:43:08 +0300

    all

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  emailInput.value = formData.email || "";
+  messageInput.value = formData.message || "";
+}
+
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trim(); 
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+
+  console.log(formData);
+
+  
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+});
